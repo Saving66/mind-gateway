5fc84581ad9b7b5e87ac5bd01eba8ecafb201a6b
saving <xiaohuangdiop@qq.com>
2023-08-27 18:17:07 +0800

    Initial

---
+++ src/test/java/com/saving/mind/gateway/MindGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saving.mind.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
