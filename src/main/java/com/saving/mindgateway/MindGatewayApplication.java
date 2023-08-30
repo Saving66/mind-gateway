43a23bb618fea5f55575bf7fb02a457a90c952fa
saving <xiaohuangdiop@qq.com>
2023-08-30 11:24:34 +0800

    Initial commit

---
+++ src/main/java/com/saving/mindgateway/MindGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.saving.mindgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saving/mindgateway/MindGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saving.mindgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
